f9c2a588a183b5bdd1a4cd67d8ec2b6a61687e97
Félix Étienne Labrie <48634914+felixlabrie@users.noreply.github.com>
2022-09-02 11:16:01 -0400

    initial commit

---
+++ MulticounterApp/MulticounterApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MulticounterApp
+//
+//  Created by Félix Étienne Labrie on 2022-09-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MulticounterApp/MulticounterApp/MulticounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MulticounterAppApp.swift
+//  MulticounterApp
+//
+//  Created by Félix Étienne Labrie on 2022-09-02.
+//
+
+import SwiftUI
+
+@main
+struct MulticounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
